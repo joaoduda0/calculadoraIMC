b3c0839eb33daf9cb94af5f5d84f5e6be12b4988
joaoduda0 <joaoeduardomc@hotmail.com>
2024-11-29 19:45:55 -0300

    Validar campos vazio

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,23 @@ class MainActivity : AppCompatActivity() {
         val bntcalcular = findViewById<Button>(R.id.bnt_calcular)
 
         bntcalcular.setOnClickListener {
-            val altura: Float = edtaltura.text.toString().toFloat()
-            val peso: Float = edtpeso.text.toString().toFloat()
-            val resultado = peso/(altura+altura)
+            val alturaStr: String = edtaltura.text.toString()
+            val pesoStr: String = edtpeso.text.toString()
 
-            println(resultado)
+            if(pesoStr == "" || alturaStr == "") {
+                Snackbar.make(
+                    edtpeso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+
+            }else{
+                val altura = alturaStr.toFloat()
+                val peso = pesoStr.toFloat()
+                val resultado = peso/(altura+altura)
+
+
+            }
 
         }
 
